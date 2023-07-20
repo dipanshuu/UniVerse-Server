f8dcaa3cf359083b5ba4399051072e7af0a32f19
dipanshuu <dipanshuu0427@gmail.com>
2023-07-20 12:43:14 +0530

    Changes in index file

--- index.js
+++ index.js
@@ -21,11 +21,8 @@ const PORT=process.env.PORT || 4001
 //middlewares
 
 app.use(express.json({limit:'10mb'}))
-console.log('here env',process.env.NODE_ENV)
-let origin='http://localhost:3000';
-if(process.env.NODE_ENV==='production'){
-    origin=process.env.CORS_ORIGIN
-}
+
+
 app.use(cors({
     credentials:true,
     origin:process.env.CORS_ORIGIN
